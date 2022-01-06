5cf3174eb6b3bcdd82d6080c0514a24e39e59f51
suryo <suryoatm@gmail.com>
2022-01-06 14:50:41 +0700

    example add kabupaten

--- pages/coba2.vue
+++ pages/coba2.vue
@@ -1,10 +1,56 @@
 <template>
   <section class="section">
     <div class>
-      tes ini coba 2
+      <div class="row">
+        <div class="col-12">
+          <div class="card">
+            <div class="card-header">
+              <h4>NEW KABUPATEN</h4>
+            </div>
+            <div class="card-body">
+              <form
+                @submit="store"
+                method="POST"
+                action="#"
+                class="needs-validation"
+                novalidate=""
+              >
+              <div class="form-group">
+                      <label>Provinsi</label>
+                      <select class="form-control">
+                        <option>Option 1</option>
+                        <option>Option 2</option>
+                        <option>Option 3</option>
+                      </select>
+                    </div>
+                <div class="form-group">
+                  <label>Kabupaten</label>
+                  <input type="text" v-model="provinsi.provinsi" class="form-control" />
+                </div>
+
+                <div class="form-group">
+                  <label
+                    class="
+                      col-form-label
+                      text-md-right
+                      col-12 col-md-3 col-lg-3
+                    "
+                  ></label>
+                  <div class="col-sm-12 col-md-7">
+                    <button type="submit" class="btn btn-primary">
+                      Simpan
+                    </button>
+                  </div>
+                </div>
+              </form>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   </section>
 </template>
+
 
 <script>
 export default {
@@ -12,29 +58,38 @@ export default {
   components: {},
   data() {
     return {
-      features: [
-        {
-          icon: "arrange-bring-to-front",
-          title: "Public",
-          content: `<span>No other internal dependency</span>`,
-          link: "/public"
-        },
-
-        {
-          icon: "github-circle",
-          title: "Refview",
-          content: `<span>Open source on <a href="https://github.com/buefy/buefy"> GitHub</a></span>`,
-          link: "/refview"
-        },
-        {
-          icon: "cellphone-link",
-          title: "Advanced Search",
-          content: `<span><b class="has-text-grey">Every</b> component is responsive</span>`,
-          link: "/coach"
-        }
-      ]
+      //state post
+      provinsi: {
+        provinsi: "",
+      },
+      //state validation
+      validation: [],
     };
-  }
+  },
+
+  methods: {
+    //method "store"
+    async store(e) {
+      e.preventDefault();
+
+      //send data ke Rest API
+      await this.$axios
+        .post("/api/provinsi", {
+          //data yang dikirim ke server
+          provinsi: this.provinsi.provinsi,
+        })
+        .then(() => {
+          //redirect ke route "post"
+          this.$router.push({
+            name: "provinsi",
+          });
+        })
+        .catch((error) => {
+          //assign validation
+          this.validation = error.response.data;
+        });
+    },
+  },
 };
 </script>
 
